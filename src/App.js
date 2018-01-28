cdea17b249d2825f060c22e1f92b28bc671652ea
Thacher <thachert1d@gmail.com>
2018-01-27 18:32:24 -0800

    bootstrap branch updates

--- src/App.js
+++ src/App.js
@@ -69,6 +69,9 @@ class App extends Component {
               </Nav>
             </Navbar.Collapse>
           </Navbar>
+          <Body>
+            
+          </Body>
         )}
       />
     );
